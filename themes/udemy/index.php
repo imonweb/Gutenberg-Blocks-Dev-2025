c74c71fbefb84b94aa203c059a45208a4baf022a
Imon <contact@imonweb.co.uk>
2024-11-09 22:23:03 +0000

    theme

--- themes/udemy/index.php
+++ themes/udemy/index.php
@@ -1,2 +1,15 @@
-<?php
-// Silence is golden.
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+  <title>Document</title>
+</head>
+<body <?php body_class('example');?> >
+  <?php wp_body_open(); ?>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+  <?php wp_footer(); ?>
+</body>
+</html>
